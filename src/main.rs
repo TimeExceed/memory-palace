12a3ca0d1ff9914fac2f989bb8325d8562b4e93f
Taoda <tyf00@aliyun.com>
2024-12-31 15:20:00 +0800

    add --completion

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use chrono::prelude::*;
-use clap::Parser;
+use clap::{crate_name, crate_version, value_parser, Arg, ArgAction, Command};
+use clap_complete::aot as completion;
 use log::*;
 use memory_palace::*;
 use std::cell::RefCell;
@@ -12,7 +13,7 @@ fn main() {
         .adaptive_format_for_stderr(flexi_logger::AdaptiveFormat::Detailed)
         .start()
         .unwrap();
-    let args = Args::parse();
+    let args = parse_args();
     let items = read_file(&args.file_name);
     let now = Utc::now();
     let selected = if let Some(ref selected_in) = args.selected_in {
@@ -33,27 +34,91 @@ fn main() {
     }
 }
 
-/// Pick up something in the memory palace.
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+fn parse_args() -> Args {
+    const COMPLETION: &str = "COMPLETION";
+    const FILE_NAME: &str = "FILE_NAME";
+    const TAKE: &str = "TAKE";
+    const DRY_RUN: &str = "DRY-RUN";
+    const SELECTED_IN: &str = "SELECTED-IN";
+    const SELECTED_OUT: &str = "SELECTED-OUT";
+    let mut cmd = Command::new(crate_name!())
+        .about("Pick up something in the memory palace.")
+        .version(crate_version!())
+        .arg(
+            Arg::new(COMPLETION)
+                .value_name("SHELL")
+                .help("Generate the completion file.")
+                .long("completion")
+                .action(ArgAction::Set)
+                .value_parser(value_parser!(completion::Shell)),
+        )
+        .arg(
+            Arg::new(FILE_NAME)
+                .help("the file of a memory palace.")
+                .action(ArgAction::Set)
+                .required_unless_present(COMPLETION),
+        )
+        .arg(
+            Arg::new(TAKE)
+                .value_name("N")
+                .help("Take at most <N> things to remember.")
+                .long("take")
+                .action(ArgAction::Set)
+                .value_parser(value_parser!(usize)),
+        )
+        .arg(
+            Arg::new(DRY_RUN)
+                .help("Do everything except writing back.")
+                .long("dry-run")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new(SELECTED_IN)
+                .value_name("SELECTED-FILE")
+                .help("Read back the selected items.")
+                .long("selected-in")
+                .action(ArgAction::Set),
+        )
+        .arg(
+            Arg::new(SELECTED_OUT)
+                .value_name("SELECTED-FILE")
+                .help("Write out the selected items.")
+                .long("selected-out")
+                .action(ArgAction::Set),
+        );
+    let matches = cmd.clone().get_matches();
+    if let Some(gen) = matches.get_one::<completion::Shell>(COMPLETION).copied() {
+        let cmd_name = cmd.get_name().to_string();
+        completion::generate(gen, &mut cmd, cmd_name, &mut std::io::stdout());
+        std::process::exit(0);
+    }
+    let file_name = matches.get_one::<String>(FILE_NAME).unwrap().clone();
+    let take = matches.get_one::<usize>(TAKE).copied();
+    let dry_run = matches.get_flag(DRY_RUN);
+    let selected_in = matches.get_one::<String>(SELECTED_IN).cloned();
+    let selected_out = matches.get_one::<String>(SELECTED_OUT).cloned();
+    Args {
+        file_name,
+        dry_run,
+        take,
+        selected_in,
+        selected_out,
+    }
+}
+
 struct Args {
     /// the file of a memory palace.
-    #[arg()]
     file_name: String,
 
     /// Take at most `TAKE` things to remember.
-    #[arg(long)]
     take: Option<usize>,
 
     /// Do everything except writing back.
-    #[arg(long)]
     dry_run: bool,
 
     /// Write out the selected items.
-    #[arg(long)]
     selected_out: Option<String>,
 
     /// Read back the selected items.
-    #[arg(long)]
     selected_in: Option<String>,
 }
